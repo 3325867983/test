067cebaa52e0a9d48459ade6ef18cee785f1d471
Xiao <3325867983@qq.com>
2023-05-04 11:19:30 +0800

    test1 hello word 1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("2");
+        System.out.println("Hello Word 1");
+        System.out.println("Hello Word 1");
+        System.out.println("Hello Word 1");
+        System.out.println("Hello Word 1");
     }
 }
